4d5d7dd35fe19e4575dbcc398cd11f915b10867a
unknown <chris.n.gutierrez@gmail.com>
2019-10-12 17:54:12 -0700

    First commit

---
+++ BoardState.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeGame
+{
+    class BoardState
+    {
+        private string One; //open, X, O
+        private string Two;
+        private string Three;
+        private string Four;
+        private string Five;
+        private string Six;
+        private string Seven;
+        private string Eight;
+        private string Nine; 
+
+        public BoardState() // Fix the casing on the below methods
+        {
+            this.One = "open";
+            this.Two = "open";
+            this.Three = "open";
+            this.Four = "open";
+            this.Five = "open";
+            this.Six = "open";
+            this.Seven = "open";
+            this.Eight = "open";
+            this.Nine = "open";
+        }
+
+        public void updateBoardState(int cellNumber, string currentPlayer) //updates the board state
+        {
+            string CurrentPlayer = currentPlayer;
+
+            //string player = Game.player;
+            //string CellToString = "";
+            switch (cellNumber)
+            {
+                case 1:
+                    SetOne(CurrentPlayer);
+                    break;
+
+                case 2:
+                    SetTwo(CurrentPlayer);
+                    break;
+
+                case 3:
+                    SetThree(CurrentPlayer);
+                    break;
+
+                case 4:
+                    SetFour(CurrentPlayer);
+                    break;
+
+                case 5:
+                    SetFive(CurrentPlayer);
+                    break;
+
+                case 6:
+                    SetSix(CurrentPlayer);
+                    break;
+
+                case 7:
+                    SetSeven(CurrentPlayer);
+                    break;
+
+                case 8:
+                    SetEight(CurrentPlayer);
+                    break;
+
+                case 9:
+                    SetNine(CurrentPlayer);
+                    break;
+            }
+        }
+        public string GetOne() 
+        {
+            return One;
+        }
+
+        public void SetOne(string CurrentPlayer) //USE THIS TO CHECK IF THERE IS ANYTHING THERE FIRST!!!!!!!!!!!!!!!!!!
+        {
+            //(String.Equals(this.One, "open", StringComparison.OrdinalIgnoreCase)) is a standard way to do string comparison like below
+            if (this.One == "open")
+            {
+                this.One = CurrentPlayer;
+            } 
+            else 
+            { 
+                Console.Clear(); 
+            }
+        }
+
+        public string GetTwo()
+        {
+            return Two;
+        }
+
+        public void SetTwo(string CurrentPlayer)
+        {
+            Two = CurrentPlayer;
+        }
+
+        public string GetThree()
+        {
+            return Three;
+        }
+
+        public void SetThree(string CurrentPlayer)
+        {
+            Three = CurrentPlayer;
+        }
+
+        public string GetFour()
+        {
+            return Four;
+        }
+
+        public void SetFour(string CurrentPlayer)
+        {
+            Four = CurrentPlayer;
+        }
+
+        public string GetFive()
+        {
+            return Five;
+        }
+
+        public void SetFive(string CurrentPlayer)
+        {
+            Five = CurrentPlayer;
+        }
+
+        public string GetSix()
+        {
+            return Six;
+        }
+
+        public void SetSix(string CurrentPlayer)
+        {
+            Six = CurrentPlayer;
+        }
+
+        public string GetSeven()
+        {
+            return Seven;
+        }
+
+        public void SetSeven(string CurrentPlayer)
+        {
+            Seven = CurrentPlayer;
+        }
+
+        public string GetEight()
+        {
+            return Eight;
+        }
+
+        public void SetEight(string CurrentPlayer)
+        {
+            Eight = CurrentPlayer;
+        }
+
+        public string GetNine()
+        {
+            return Nine;
+        }
+
+        public void SetNine(string CurrentPlayer)
+        {
+            Nine = CurrentPlayer;
+        }
+
+        public string WinChecker()
+        {
+            if (
+                this.One == "X" && this.Two == "X" && this.Three == "X" ||
+                this.Four == "X" && this.Five == "X" && this.Six == "X" ||
+                this.Seven == "X" && this.Eight == "X" && this.Nine == "X" ||
+                this.One == "X" && this.Four == "X" && this.Seven == "X" ||
+                this.Two == "X" && this.Five == "X" && this.Eight == "X" ||
+                this.Three == "X" && this.Six == "X" && this.Nine == "X" ||
+                this.One == "X" && this.Five == "X" && this.Nine == "X" ||
+                this.Three == "X" && this.Five == "X" && this.Seven == "X"
+               )
+            {
+                return "X is the winner";
+            }
+            if (
+                this.One == "O" && this.Two == "O" && this.Three == "O" ||
+                this.Four == "O" && this.Five == "O" && this.Six == "O" ||
+                this.Seven == "O" && this.Eight == "O" && this.Nine == "O" ||
+                this.One == "O" && this.Four == "O" && this.Seven == "O" ||
+                this.Two == "O" && this.Five == "O" && this.Eight == "O" ||
+                this.Three == "O" && this.Six == "O" && this.Nine == "O" ||
+                this.One == "O" && this.Five == "O" && this.Nine == "O" ||
+                this.Three == "O" && this.Five == "O" && this.Seven == "O"
+               )
+            {
+                return "O is the winner";
+            }
+
+            return "";
+        }
+    }
+}
---
+++ Game.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeGame
+{
+    class Game
+    {
+        bool playerState = true;
+        string CurrentPlayer;
+        int gameTurn = 1;
+        bool winner;
+        bool active = true;
+
+        public Game()
+        {
+            Board board = new Board();
+            BoardState boardState = new BoardState();
+            while (active)
+            {
+                //PlaceAt("", 0, 4);
+                Console.WriteLine("Input desired marker position: ");
+                string PlayerInput = Console.ReadLine();
+                int Cell;
+                bool ValidInput = int.TryParse(PlayerInput, out Cell);
+                if (ValidInput == true)
+                {
+                    boardState.updateBoardState(Cell, CurrentPlayer);   //attempts to set X and O position in boardState instance
+                    Console.WriteLine(boardState.GetOne());     //checking if boardState updated when 1 was input
+                    PlaceMarker(Cell);
+                    string IsThereAWinner = boardState.WinChecker();
+                    PlaceAt("", 0, 5);
+                    Console.WriteLine(IsThereAWinner);
+                    if (playerState == true) playerState = false;
+                    else playerState = true;
+                }
+                else
+                {
+                    PlaceAt("", 0, 5);
+                    Console.WriteLine("Not a valid input, please input a valid cell number:");
+                }
+
+                
+            }
+        }
+        
+        protected static int origRow;
+        protected static int origCol;
+
+        protected static void PlaceAt(string s, int x, int y) //Places marker at, string s, position (x, y)
+        {
+            try
+            {
+                Console.SetCursorPosition(origCol + x, origRow + y);
+                Console.Write(s);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.Clear();
+                Console.WriteLine(e.Message);
+            }
+        }
+
+
+        public void PlaceMarker(int cell) //Places player marker in selected square
+        {
+            if (playerState) CurrentPlayer = "X";
+            else CurrentPlayer = "O";
+            switch (cell)
+            {
+                case 1:
+                    PlaceAt(CurrentPlayer, 1, 0);
+                    break;
+
+                case 2:
+                    PlaceAt(CurrentPlayer, 5, 0);
+                    break;
+
+                case 3:
+                    PlaceAt(CurrentPlayer, 9, 0);
+                    break;
+
+                case 4:
+                    PlaceAt(CurrentPlayer, 1, 2);
+                    break;
+
+                case 5:
+                    PlaceAt(CurrentPlayer, 5, 2);
+                    break;
+
+                case 6:
+                    PlaceAt(CurrentPlayer, 9, 2);
+                    break;
+
+                case 7:
+                    PlaceAt(CurrentPlayer, 1, 4);
+                    break;
+
+                case 8:
+                    PlaceAt(CurrentPlayer, 5, 4);
+                    break;
+
+                case 9:
+                    PlaceAt(CurrentPlayer, 9, 4);
+                    break;
+            }
+        }
+    }
+}
