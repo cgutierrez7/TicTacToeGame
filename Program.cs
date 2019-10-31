868bec1f780c22757a09e5b164f61844d696fd20
unknown <chris.n.gutierrez@gmail.com>
2019-10-31 09:29:58 -0700

    Fixed BoardState bug

--- Program.cs
+++ Program.cs
@@ -7,6 +7,19 @@ namespace TicTacToeGame
         static void Main(string[] args)
         {
             Game game = new Game();
+            while (true)
+            {
+                Console.WriteLine("Play Again? (y/n)");
+                char quit;
+                char.TryParse(Console.ReadLine(), out quit);
+                quit = char.ToLower(quit);
+                if (quit == 'y')
+                    game = new Game();
+                else if (quit == 'n')
+                    return;
+                else 
+                    continue;
+            }
         }
     }
 }
